74699ef8e50db6446450bd7922f048bd245fc8ba
Abhishek Fulzele <be.bune13@gmail.com>
2018-07-19 07:44:17 +0530

    static Image

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes:Recipe[] = [
-    new Recipe('Test Recipe', 'Simple Recipe', 'https://pixabay.com/get/ea34b20d21f3023ed1584d05fb0938c9bd22ffd41cb2104894f6c878af/strawberry-3132973_1280.jpg'),
-    new Recipe('Test Recipe', 'Simple Recipe', 'https://pixabay.com/get/ea34b20d21f3023ed1584d05fb0938c9bd22ffd41cb2104894f6c878af/strawberry-3132973_1280.jpg')
+    new Recipe('Test Recipe', 'Simple Recipe', '../../../assets/img/recipe1.jpg'),
+    new Recipe('Test Recipe', 'Simple Recipe', '../../../assets/img/recipe1.jpg')
   ];
 
   constructor() { }
