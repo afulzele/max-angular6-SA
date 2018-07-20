29cb6534532b83003d62026c88e2c830bf35ba60
Abhishek Fulzele <be.bune13@gmail.com>
2018-07-20 06:03:17 +0530

    Emitters and Listeners

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes:Recipe[] = [
     new Recipe('Test Recipe', 'Simple Recipe', '../../../assets/img/recipe1.jpg'),
-    new Recipe('Test Recipe', 'Simple Recipe', '../../../assets/img/recipe1.jpg')
+    new Recipe('Test Recipe 2', 'Simple Recipe 2', '../../../assets/img/recipe1.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);  
   }
 
 }
